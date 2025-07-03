659fa198dff63b4c43b60ee85d0323051285a183
PKyes44 <rugate23@gmail.com>
2025-07-03 18:10:22 +0900

    feat: page routing

---
+++ View/ViewLogControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace admin_client.View
+{
+    /// <summary>
+    /// Interaction logic for ViewLogControl.xaml
+    /// </summary>
+    public partial class ViewLogControl : UserControl
+    {
+        public ViewLogControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
